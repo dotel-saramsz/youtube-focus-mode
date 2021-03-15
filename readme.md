3f16ae39f2dc035d45e93f8ece1397f4762a3cfb
Saramsha Dotel <saramsha.d@gmail.com>
2021-03-15 20:32:31 +0545

    Initial commit: Create folder structure

---
+++ readme.md
@@ -0,0 +1,3 @@
+# No Distraction YouTube
+
+A Chrome extension that blocks and hides YouTube videos that belong to *distractive* categories. 
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import logo from "./assets/logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Watch YouTube without distractions!!!
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
